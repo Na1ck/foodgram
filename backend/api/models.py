0351caae4532b31503dd7754e0a8626f2139d6d8
Nikita Kudryavtsev <Naick2018@gmail.com>
2025-10-03 00:55:37 +0300

    V 0.2

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,6 @@
-from django.utils import timezone
-from django.db import models
 from django.contrib.auth import get_user_model
+from django.db import models
+from django.utils import timezone
 
 User = get_user_model()
 
