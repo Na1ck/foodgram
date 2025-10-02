bd007b67e68ea159c11151a8235f60e324730afd
Nikita Kudryavtsev <Naick2018@gmail.com>
2025-10-03 00:45:45 +0300

    add slugify in tags model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,5 +1,6 @@
 from django.utils import timezone
 from django.db import models
+from django.utils.text import slugify
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -11,8 +12,10 @@ class Tag(models.Model):
     slug = models.CharField(max_length=32, unique=True,
                             null=True, blank=True, verbose_name='Слаг')
 
-    def __str__(self):
-        return self.name
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     class Meta:
         verbose_name = 'Тег'
