a19d5ac80e971b5eb3b449a29bd0158380d1178d
Nikita Kudryavtsev <Naick2018@gmail.com>
2025-10-08 10:39:11 +0300

    V 0.9

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -3,8 +3,37 @@ from ingredients.models import Ingredient
 from recipes.models import Recipe, RecipeIngredient, RecipeTag
 from tags.models import Tag
 
-admin.site.register(Tag)
-admin.site.register(Ingredient)
-admin.site.register(Recipe)
-admin.site.register(RecipeTag)
-admin.site.register(RecipeIngredient)
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    list_display_links = ('name',)
+    search_fields = ('name', 'slug')
+    list_per_page = 20
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit')
+    search_fields = ('name',)
+    list_filter = ('measurement_unit',)
+    list_per_page = 20
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'cooking_time')
+    list_filter = ('tags', 'author')
+    search_fields = ('name', 'description')
+    filter_horizontal = ('tags',)
+
+
+@admin.register(RecipeIngredient)
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'ingredient', 'amount')
+    list_filter = ('recipe', 'ingredient')
+
+
+@admin.register(RecipeTag)
+class RecipeTagAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'tag')
