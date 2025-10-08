f4e61f2b9cc6c8ef4ac43504501a5092c6640184
Nikita Kudryavtsev <Naick2018@gmail.com>
2025-10-08 12:48:25 +0300

    pagination class

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -109,7 +109,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 6,
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
