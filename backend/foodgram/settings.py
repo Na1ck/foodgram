37fb47c0967788672c7e56b93fa346e7d7187174
Nikita Kudryavtsev <Naick2018@gmail.com>
2025-10-19 11:19:17 +0300

    change settings

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -140,8 +140,6 @@ AUTH_USER_MODEL = 'users.User'
 
 CSRF_TRUSTED_ORIGINS = os.getenv('DJANGO_CSRF_TRUSTED_ORIGINS', '').split(',')
 
-ALLOWED_HOSTS = ['*']
-
 CORS_ORIGIN_WHITELIST = [ 
     'http://localhost:3000' 
 ]
